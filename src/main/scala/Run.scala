37cff51aa347d80730ef5e24b01239bc47759354
LKRHLQ <LKRHLQ@AF330619.au.amp.local>
2018-03-15 14:59:35 +1100

    handle parameters

--- src/main/scala/Run.scala
+++ src/main/scala/Run.scala
@@ -4,13 +4,13 @@
 object Run {
 
   def main(args: Array[String]): Unit = {
-    if (args.length < 2) {
-      println("Please provide input string and minimum occurence")
+    if (args.length < 1) {
+      println("Please provide input string and optional minimum occurence")
       System.exit(1)
     } else {
       try {
         val compress = new StringCompressor
-        val result = compress.compressStr(args(0), Some(args(1).toInt))
+        val result = compress.compressStr(args(0), if(args.length == 2) Some(args(1).toInt) else None)
         println(s"$result")
       } catch {
         case e: NumberFormatException => println(s"Invalid minimum occurence supplied - $e")
